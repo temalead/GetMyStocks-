1a3c50db4e0d7ed808abb4028534a7013f2cc3b8
nikulin-me <itdetectiv@gmail.com>
2022-02-14 18:58:52 +0400

    Replaced double div to big decimal. Also added modules to run spring boot tests

--- src/test/java/bot/service/StockServiceTest.java
+++ src/test/java/bot/service/StockServiceTest.java
@@ -1,12 +1,16 @@
 package bot.service;
 
 import bot.exception.NotFoundStockException;
-import junit.framework.TestCase;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 public class StockServiceTest {
     @Autowired
     private StockService service;
@@ -14,8 +18,8 @@ public class StockServiceTest {
     @Test
     public void testGetLastDividendByTicker() throws NotFoundStockException {
 
-        Long gazp = service.getLastDividendByTicker("GAZP");
-        System.out.println(gazp);
+        BigDecimal gazp = service.getLastDividendByTicker("FEES");
+        System.out.println(gazp.doubleValue());
     }
 
 }
