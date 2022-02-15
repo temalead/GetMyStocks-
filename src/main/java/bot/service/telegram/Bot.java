35f6735919f7fcf20f8dddb975dbb696865dfd9a
nikulin-me <itdetectiv@gmail.com>
2022-02-15 22:18:22 +0400

    Temp gag in update handler

--- src/main/java/bot/service/telegram/Bot.java
+++ src/main/java/bot/service/telegram/Bot.java
@@ -8,6 +8,7 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 @Component
 @RequiredArgsConstructor
@@ -32,7 +33,7 @@ public class Bot extends TelegramLongPollingBot {
         }
     }
 
-    private void handleMessage(Message message) {
-
+    private void handleMessage(Message message) throws TelegramApiException {
+        execute(SendMessage.builder().chatId(String.valueOf(message.getChatId())).text(message.getText()).build());
     }
 }
---
+++ src/main/java/bot/service/telegram/utils/ShareInfoSender.java
@@ -0,0 +1,9 @@
+package bot.service.telegram.utils;
+
+import org.telegram.telegrambots.meta.api.objects.Message;
+
+public abstract class ShareInfoSender {
+    public Message sendMessage(){
+        return null;
+    }
+}
