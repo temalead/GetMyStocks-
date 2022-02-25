77687c4aebc461eda09c91fc1a7977fb82dc9a13
nikulin-me <itdetectiv@gmail.com>
2022-02-25 20:59:37 +0400

    Added menu

--- src/main/java/bot/service/telegram/handlers/UpdateHandler.java
+++ src/main/java/bot/service/telegram/handlers/UpdateHandler.java
@@ -4,8 +4,6 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
-import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 @RequiredArgsConstructor
@@ -19,9 +17,8 @@ public class UpdateHandler {
         if (update.hasCallbackQuery()) {
             return callbackHandler.handleCallbackQuery(update.getCallbackQuery());
         } else {
-            Message message = update.getMessage();
             log.info("It`s a message");
-            return SendMessage.builder().chatId(String.valueOf(message.getChatId())).text("Text").build();
+            return messageHandler.handleMessage(update.getMessage());
         }
     }
 }
