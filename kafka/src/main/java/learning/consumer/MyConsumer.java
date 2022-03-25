e5831aa2875fc74d2dfadac84d728dd70fa8e797
nikulin-me <itdetectiv@gmail.com>
2022-03-25 20:19:16 +0400

    Dockerized

--- kafka/src/main/java/learning/consumer/MyConsumer.java
+++ kafka/src/main/java/learning/consumer/MyConsumer.java
@@ -15,16 +15,17 @@ import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_COMMIT_INTER
 import static org.apache.kafka.clients.consumer.ConsumerConfig.AUTO_OFFSET_RESET_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG;
+import static org.apache.kafka.clients.consumer.ConsumerConfig.MAX_POLL_RECORDS_CONFIG;
 import static org.apache.kafka.clients.consumer.ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG;
 
 public class MyConsumer {
     private final KafkaConsumer<String, String> kafkaConsumer;
 
     public static final String TOPIC_NAME = "MyTopic";
-    public static final String GROUP_ID_CONFIG_NAME ="myKafkaConsumerGroup";
+    public static final String GROUP_ID_CONFIG_NAME = "myKafkaConsumerGroup";
     public static final int MAX_POLL_INTERVAL_MS = 300;
 
-    public MyConsumer(String bootstrapServers)  {
+    public MyConsumer(String bootstrapServers) {
         Properties props = new Properties();
         props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
         props.put(GROUP_ID_CONFIG, GROUP_ID_CONFIG_NAME);
@@ -32,11 +33,11 @@ public class MyConsumer {
         props.put(ENABLE_AUTO_COMMIT_CONFIG, "true");
         props.put(AUTO_COMMIT_INTERVAL_MS_CONFIG, "10");
         props.put(AUTO_OFFSET_RESET_CONFIG, "latest");
-        props.put(SESSION_TIMEOUT_MS_CONFIG,10000);
+        props.put(SESSION_TIMEOUT_MS_CONFIG, 10000);
         props.put(KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
         props.put(VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
 
-        // props.put(MAX_POLL_RECORDS_CONFIG, 3);
+        props.put(MAX_POLL_RECORDS_CONFIG, 3); //how many values consumer eats per second
         // props.put(MAX_POLL_INTERVAL_MS_CONFIG, MAX_POLL_INTERVAL_MS);
 
         kafkaConsumer = new KafkaConsumer<>(props);
@@ -47,11 +48,11 @@ public class MyConsumer {
         return kafkaConsumer;
     }
 
-    public String makeGroupInstanceIdConfig()  {
+    public String makeGroupInstanceIdConfig() {
         try {
             var hostName = InetAddress.getLocalHost().getHostName();
             return String.join("-", GROUP_ID_CONFIG_NAME, hostName, String.valueOf(new Random().nextInt(100_999_999)));
-        } catch(Exception ex) {
+        } catch (Exception ex) {
             throw new ConsumerException("can't make GroupInstanceIdConfig", ex);
         }
     }
--- temp_app/src/main/java/bot/InvestServiceRunner.java
+++ temp_app/src/main/java/bot/InvestServiceRunner.java
@@ -3,6 +3,7 @@ package bot;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 
@@ -11,6 +12,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @EnableEncryptableProperties
 @EnableAsync
 @EnableRedisRepositories
+@EnableEurekaClient
 public class InvestServiceRunner {
     public static void main(String[] args) {
         SpringApplication.run(InvestServiceRunner.class,args);
