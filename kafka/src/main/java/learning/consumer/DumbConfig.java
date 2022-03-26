723c62b5ec0d712eaaf356d89072e74d9f2f4293
nikulin-me <itdetectiv@gmail.com>
2022-03-26 12:10:33 +0400

    Connected 2 microservices by eureka

---
+++ kafka/src/main/java/learning/consumer/DumbConfig.java
@@ -0,0 +1,13 @@
+package learning.consumer;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class DumbConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){return new RestTemplate();}
+}
