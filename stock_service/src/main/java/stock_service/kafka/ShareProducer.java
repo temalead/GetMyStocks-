0d09ff9c0c626964a75f151152544ae01dba9ec5
nikulin-me <itdetectiv@gmail.com>
2022-06-12 23:21:38 +0400

    Revert "Message sending to share and bond response topics from stock service"
    This reverts commit 855c007d

--- stock_service/src/main/java/stock_service/kafka/ShareProducer.java
+++ stock_service/src/main/java/stock_service/kafka/ShareProducer.java
@@ -7,6 +7,7 @@ import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import stock_service.config.TopicsProperties;
 import stock_service.entity.MyShare;
+import stock_service.repository.ShareRepository;
 import stock_service.service.ShareService;
 
 @Service
@@ -14,17 +15,15 @@ import stock_service.service.ShareService;
 public class ShareProducer {
 
     private final ShareService service;
-    private final KafkaTemplate<String, String> kafkaTemplate;
-    private final TopicsProperties properties;
 
-    private final ObjectMapper mapper;
+    private final ShareRepository repository;
+
 
     @SneakyThrows
     public void sendResponse(String requestedShare) {
 
         MyShare resultShare = service.getInfo(requestedShare);
-        String value = mapper.writeValueAsString(resultShare);
 
-        kafkaTemplate.send(properties.getShare(), value);
+        repository.save(resultShare);
     }
 }
