daca05299d115815bdf535021baef58d22233e09
nikulin-me <itdetectiv@gmail.com>
2022-06-12 23:27:14 +0400

    Revert changes in kafka messaging share message from stock service

--- telegram_service/src/main/java/bot/kafka/ShareResponseConsumer.java
+++ telegram_service/src/main/java/bot/kafka/ShareResponseConsumer.java
@@ -1,19 +1,10 @@
 package bot.kafka;
 
-import bot.service.sender.ShareRedundant;
-import bot.service.sender.ShareSender;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.clients.consumer.ConsumerRecords;
-import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
-
-import javax.annotation.PostConstruct;
-import java.time.Duration;
-import java.util.Collections;
-import java.util.Properties;
 
 @Service
 @Slf4j
@@ -21,13 +12,13 @@ import java.util.Properties;
 public class ShareResponseConsumer {
     private final String SHARE_RES_TOPIC = "share_res.topic";
 
-    private final ShareRedundant shareRedundant;
+    //private final ShareRedundant shareRedundant;
 
     @SneakyThrows
     @KafkaListener(id = "share_res", topics = SHARE_RES_TOPIC)
     public void getShareResponse(String message) {
 
-        shareRedundant.setMessage(message);
+        //  shareRedundant.setMessage(message);
 
     }
 }
--- telegram_service/src/main/java/bot/service/sender/ShareSender.java
+++ telegram_service/src/main/java/bot/service/sender/ShareSender.java
@@ -2,8 +2,8 @@ package bot.service.sender;
 
 
 import bot.entity.MyShare;
-import bot.entity.dto.DividendDto;
-import bot.entity.dto.DividendListDto;
+import bot.entity.dto.Dividend;
+import bot.entity.dto.DividendList;
 import bot.exception.ShareNotFoundException;
 import bot.repository.ShareRepository;
 import lombok.RequiredArgsConstructor;
@@ -23,7 +23,7 @@ public class ShareSender implements SecuritySender {
     public SendMessage getInfo(Message message) {
         String chatId = message.getChatId().toString();
         String ticker = message.getText();
-        MyShare info = repository.findById(ticker).orElseThrow(()->new ShareNotFoundException(ticker));
+        MyShare info = repository.findById(ticker).orElseThrow(() -> new ShareNotFoundException(ticker));
         String result = createMessage(info);
 
         return SendMessage.builder().chatId(chatId).text(result).build();
@@ -39,17 +39,17 @@ public class ShareSender implements SecuritySender {
         return price + "\n" + dividendMessage;
     }
 
-    private String createDividendMessage(DividendListDto dividends) {
+    private String createDividendMessage(DividendList dividends) {
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("Dividends per year\n");
-        if (dividends.getDividends()==null){
+        if (dividends==null){
             return stringBuilder.append("Dividends were not paid").toString();
         }
-        List<DividendDto> list = dividends.getDividends();
+        stringBuilder.append("Dividends per year\n");
+        List<Dividend> list = dividends.getDividends();
         if (list.isEmpty()) {
             return "Dividends were not paid";
         } else {
-            for (DividendDto dto : list) {
+            for (Dividend dto : list) {
                 BigDecimal payment1 = dto.getPayment();
                 String payment = "Payment: " + payment1 + "\n";
                 String date = "Date: " + dto.getPaymentDate() + "\n";
